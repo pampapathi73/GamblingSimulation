4cb9b945e57aadd585ac4734bd01798ac88a6c0e
pampapathi73 <pampapathik32@gmail.com>
2021-06-10 14:25:48 +0530

    [pampapathi] Added: gambling simulation win or loss

--- GamblingSimulations/Program.cs
+++ GamblingSimulations/Program.cs
@@ -7,9 +7,13 @@ namespace GamblingSimulations
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to gambing simulation");
+            int stakeHold = 100;
+            const int bet = 1;
+            int days = 30;
 
-
-        }
+            Usecase2 usecase2 = new Usecase2();
+            usecase2.winLoss();
+            }
     }
 
 }
---
+++ GamblingSimulations/Usecase2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblingSimulations
+{
+    class Usecase2
+    {
+        public void winLoss()
+        {
+            Random random = new Random();
+            int value = random.Next(2);
+            if (value == 1)
+            {
+                Console.WriteLine("Gambler own");
+            }
+            else
+                Console.WriteLine("Gambler Loss");
+        }
+
+    }
+}

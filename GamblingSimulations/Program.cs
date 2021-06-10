7b6134844df1dba5f5c83bc72dae0bab80af612a
pampapathi73 <pampapathik32@gmail.com>
2021-06-10 12:40:14 +0530

    [pampapathi] Added:  welcome to gambling simulation

---
+++ GamblingSimulations/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GamblingSimulations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to gambing simulation");
+            
+        }
+    }
+}
